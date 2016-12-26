f48eed69a6b35d5c262b67e0d300ca79d52de682
Chijure <chijure100x100to@gmail.com>
2016-12-26 01:20:09 -0500

    Add sweep2wake and doubletap2wake support thanks @kirito96 Signed-off-by: Vineeth Raj <contact.twn@openmailbox.org> Signed-off-by: Jhuan Reategui <chijure100x100to@gmail.com>

--- drivers/misc/mediatek/keypad/kpd.c
+++ drivers/misc/mediatek/keypad/kpd.c
@@ -26,6 +26,15 @@
 #include <linux/of_irq.h>
 #endif
 
+#ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
+#ifdef CONFIG_TOUCHSCREEN_SWEEP2WAKE
+#include <linux/input/sweep2wake.h>
+#endif
+#ifdef CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE
+#include <linux/input/doubletap2wake.h>
+#endif
+#endif
+
 #define KPD_NAME	"mtk-kpd"
 #define MTK_KP_WAKESOURCE	/* this is for auto set wake up source */
 
@@ -867,7 +876,16 @@ static int kpd_pdrv_probe(struct platform_device *pdev)
 		__set_bit(KPD_KEY_MAP, kpd_input_dev->keybit);
 #endif
 
-	kpd_input_dev->dev.parent = &pdev->dev;
+#ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
+#ifdef CONFIG_TOUCHSCREEN_SWEEP2WAKE
+	sweep2wake_setdev(kpd_input_dev);
+#endif
+#ifdef CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE
+	doubletap2wake_setdev(kpd_input_dev);
+#endif
+#endif
+
+kpd_input_dev->dev.parent = &pdev->dev;
 	r = input_register_device(kpd_input_dev);
 	if (r) {
 		printk(KPD_SAY "register input device failed (%d)\n", r);
